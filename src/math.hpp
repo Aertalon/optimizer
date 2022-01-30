e65a53d239041aa15ffca78136aba37828194275
Oliver Lee <oliverzlee@gmail.com>
2022-01-30 23:53:30 +0100

    Remove `dual_fn` and explicit derivatives

--- src/math.hpp
+++ src/math.hpp
@@ -12,29 +12,29 @@
 namespace opt {
 namespace impl {
 
-template <std::size_t N>
-inline constexpr auto exp_ = make_base_fn_<N>([]<Real T>(T x) -> T {
+template <int N>
+inline constexpr auto exp_ = make_base_fn_<N>([]<Arithmetic T>(T x) -> T {
     return series::sum_first<N>(series::geometric{
-        1U,    //
+        1,     //
         T{1},  //
-        [x](auto n) { return x / T(n); }});
+        [x](auto n) { return x / static_cast<T>(n); }});
 });
 
-template <std::size_t N>
-inline constexpr auto sin_ = make_base_fn_<N>([]<Real T>(T x) -> T {
+template <int N>
+inline constexpr auto sin_ = make_base_fn_<N>([]<Arithmetic T>(T x) -> T {
     return series::sum_first<N>(series::geometric{
-        1U,  //
-        x,   //
+        1,  //
+        x,  //
         [x](auto i) {
             const auto n = T(i);
             return (-1) * (x * x) / (2 * n) / (2 * n + 1);
         }});
 });
 
-template <std::size_t N>
-inline constexpr auto cos_ = make_base_fn_<N>([]<Real T>(T x) -> T {
+template <int N>
+inline constexpr auto cos_ = make_base_fn_<N>([]<Arithmetic T>(T x) -> T {
     return series::sum_first<N>(series::geometric{
-        1U,    //
+        1,     //
         T{1},  //
         [x](auto i) {
             const auto n = T(i);
@@ -42,35 +42,10 @@ inline constexpr auto cos_ = make_base_fn_<N>([]<Real T>(T x) -> T {
         }});
 });
 
-template <std::default_initializable F, std::default_initializable G>
-struct dual_fn {
-    constexpr dual_fn(F, G) noexcept {}
-
-    template <Dual T>
-    constexpr auto operator()(const T& x) const -> T
-    {
-        return {F{}(x.real), x.imag * G{}(x.real)};
-    }
-    template <Real T>
-    constexpr auto operator()(T x) const -> T
-    {
-        return T{F{}(x)};
-    }
-};
-
 }  // namespace impl
 
-template <std::size_t N>
-inline constexpr impl::dual_fn exp_{impl::exp_<N>, impl::exp_<N>};
-
-template <std::size_t N>
-inline constexpr impl::dual_fn sin_{impl::sin_<N>, impl::cos_<N>};
-
-template <std::size_t N>
-inline constexpr impl::dual_fn cos_{impl::cos_<N>, -impl::sin_<N>};
-
-inline constexpr auto exp = exp_<10>;
-inline constexpr auto sin = sin_<10>;
-inline constexpr auto cos = cos_<10>;
+inline constexpr auto exp = impl::exp_<10>;
+inline constexpr auto sin = impl::sin_<10>;
+inline constexpr auto cos = impl::cos_<10>;
 
 }  // namespace opt
