a79bcd2113d3f138ee03c7f34c8b02b42d42dd30
ELovisari <elovisari@gmail.com>
2022-02-12 17:24:14 +0100

    Add Frobenius norm for matrices
    Adds notion of Frobenius norm for matrices.

--- src/norms.hpp
+++ src/norms.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "math.hpp"
+#include "matrix.hpp"
+#include "matrix_ops.hpp"
 #include "spaces.hpp"
 
 namespace opt {
 
+struct Frobenius {};
 struct L2 {};
 
 namespace detail {
@@ -19,6 +22,14 @@ struct norm_fn {
         return sqrt([&v]<std::size_t... Is>(std::index_sequence<Is...>) {
             return ((v[Is] * v[Is]) + ...);
         }(std::make_index_sequence<std::tuple_size_v<V>>{}));
+    }
+
+    template <Vector V, std::size_t Cols>
+    requires(std::is_same<NormType, Frobenius>::value)
+        [[nodiscard]] constexpr auto
+        operator()(matrix<V, Cols> const& m) const -> scalar_t<V>
+    {
+        return sqrt(trace(m * m.transpose()));
     }
 };
 }  // namespace detail
--- test/norms_test.cpp
+++ test/norms_test.cpp
@@ -1,3 +1,4 @@
+#include "src/matrix.hpp"
 #include "src/norms.hpp"
 #include "src/spaces.hpp"
 
@@ -11,6 +12,7 @@ auto main() -> int
 {
     using namespace boost::ut;
 
+    using opt::matrix;
     using opt::norm;
     using opt::normalize;
     using opt::vector;
@@ -21,6 +23,18 @@ auto main() -> int
 
         expect(constant<eq(norm<opt::L2>(v), 5._f)>);
         expect(constant<eq(normalize(v), v_normalized)>);
+    };
+
+    test("spaces matrix norm, frobenius") = [] {
+        // clang-format off
+        constexpr matrix<vector<float, 3>, 2> m{
+            1.0F, 0.0F,
+            0.0F, 1.0F,
+            1.0F, 0.0F};
+        // clang-format on
+        constexpr float expected_norm{opt::sqrt(3.0F)};
+
+        expect(constant<eq(norm<opt::Frobenius>(m), expected_norm)>);
     };
 }
 
